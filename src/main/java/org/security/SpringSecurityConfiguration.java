53b8d6e286a0081f83c677ef8a4bf8b85dc777b3
Raghu <araghu.95@gmail.com>
2023-08-26 14:29:18 +0530

    dummy commit

--- src/main/java/org/security/SpringSecurityConfiguration.java
+++ src/main/java/org/security/SpringSecurityConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Spring security configuration class implemented authentication and authorization
+ *
  */
 @Configuration
 @EnableWebSecurity
